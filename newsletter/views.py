d5edd4bb1476674b9afa6b8a205cd6d36bffbfd6
Aniket Savanand <aniketpsavanand@gmail.com>
2016-10-14 15:19:53 -0700

    Views and Context Templates

--- newsletter/views.py
+++ newsletter/views.py
@@ -2,4 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-	return render(request, "home.html", {})
+	title = "Welcome Guest"  # view function variable
+	if request.user.is_authenticated():
+		title = "My Title. Welcome %s" %(request.user)
+
+	#add form here
+	context = {
+		"title" : title,  #template context variable
+	}
+	return render(request, "home.html", context)
