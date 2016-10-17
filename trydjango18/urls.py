a6a1feb7f1358d59340d85d5f89366669bb20323
Aniket Savanand <aniketpsavanand@gmail.com>
2016-10-17 01:52:29 -0700

    serving static files. changes in url.py

--- trydjango18/urls.py
+++ trydjango18/urls.py
@@ -1,5 +1,8 @@
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
+
 
 urlpatterns = [
     # Examples:
@@ -8,4 +11,11 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-]
+] 
+#+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)  # we dont want to this in production/ only in developement
+
+
+if settings.DEBUG:   /#if true we are in developement env 
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  	
+
